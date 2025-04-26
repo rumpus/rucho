16fa0992be1b6448eb99f18cdd183e16387635b2
SheriffTwinkie <declan.devito@konghq.com>
2025-04-26 19:18:05 -0400

    refactor: split handlers into organized routes

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,19 @@ use axum::{routing::{get, post}, Router, serve};
 use tower_http::trace::TraceLayer;
 use tracing_subscriber;
 use tokio::net::TcpListener;
+use routes::{get as get_routes, post as post_routes}; // <-- NEW
+
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
     let app = Router::new()
-    .route("/", get(handlers::echo::root))
-    .route("/get", get(handlers::echo::get_handler))
-    .route("/post", post(handlers::echo::post_handler)) // <--- add this line
+    .route("/", get(get_routes::root))
+    .route("/get", get(get_routes::get_handler))
+    .route("/post", post(post_routes::post_handler))
     .layer(TraceLayer::new_for_http());
+
 
 
     let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
